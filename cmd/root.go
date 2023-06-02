8d046fb6a0903bade3f8095d3b9dfd9188e6941f
Dimitris Dalianis <dimitris.dalianis@packetfabric.com>
2023-06-02 22:42:22 +0300

    it works?

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -80,7 +79,8 @@ func initConfig() {
 	}
 }
 
-func keyvalDetector() {
+func keyvalDetector() error {
+
 	// uses the current context in kubeconfig
 	// path-to-kubeconfig -- for example, /root/.kube/config
 	config, _ := clientcmd.BuildConfigFromFlags("", homedir.HomeDir()+defaultKubeConfigPath)
@@ -88,20 +88,124 @@ func keyvalDetector() {
 	// creates the clientset
 	clientset, _ := kubernetes.NewForConfig(config)
 
-	// access the API to list pods
-	pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
-
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-	//deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-	deploymentsClient := clientset.AppsV1().Deployments("flux-system")
-
-	list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
+	// List all the namespaces in the cluster.
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
-	for _, d := range list.Items {
-		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
-	}
 
+	// Foreach namespace, get configmaps, secrets and pods.
+	// Then check pod's mounts
+
+	// Foreach namespace
+	for _, namespace := range namespaces.Items {
+		// Slice to store mounted ConfigMap names
+		namespaceMountedConfigMaps := []string{}
+
+		// Slice to store mounted Secrets names
+		namespaceMountedSecrets := []string{}
+
+		// List all pods in current namespace
+		pods, err := clientset.CoreV1().Pods(namespace.GetName()).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		// List all configmaps in current namespace
+		configmaps, err := clientset.CoreV1().ConfigMaps(namespace.GetName()).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		// List all secrets in current namespace
+		secrets, err := clientset.CoreV1().Secrets(namespace.GetName()).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		// If there's no pod in current namespace any configmap/secret are not used (except defaults)
+		if len(pods.Items) == 0 {
+
+			// Print the configmaps' names
+			for _, configmap := range configmaps.Items {
+				if !isDefaultConfigMap(configmap.GetName()) {
+					fmt.Println("ConfigMap \"" + configmap.GetName() + "\" in namespace \"" + namespace.GetName() + "\" not used")
+				}
+			}
+
+			// Print the secrets' names.
+			for _, secret := range secrets.Items {
+				if !isDefaultSecret(secret.GetName()) {
+					fmt.Println("Secret \"" + secret.GetName() + "\" in namespace \"" + namespace.GetName() + "\" not used")
+				}
+			}
+		}
+
+		// If there is any pod in current namespace
+		// Iterate over the pods
+		// Check each pod's Volumes and store the configmaps and secrets mounted names on a separate slice
+		// Compare lists of configmaps/secrets with mounted ones
+		if len(pods.Items) != 0 {
+			// Foreach pod
+			for _, pod := range pods.Items {
+				// Foreach pod's volume
+				for _, volume := range pod.Spec.Volumes {
+					if volume.ConfigMap != nil {
+						namespaceMountedConfigMaps = append(namespaceMountedConfigMaps, volume.ConfigMap.Name)
+					}
+					if volume.Secret != nil {
+						namespaceMountedSecrets = append(namespaceMountedSecrets, volume.Secret.SecretName)
+					}
+				}
+			}
+		}
+
+		// I need check if configmaps/secrets of this namespace
+		// are in namespaceMountedConfigMaps/namespaceMountedSecrets
+
+		for _, configmap := range configmaps.Items {
+			if !contains(namespaceMountedConfigMaps, configmap.GetName()) {
+				if !isDefaultConfigMap(configmap.GetName()) {
+					println("ConfigMap \"" + configmap.GetName() + "\" in namespace \"" + namespace.GetName() + "\" not used")
+				}
+			}
+		}
+
+		for _, secret := range secrets.Items {
+			if !isDefaultSecret(secret.GetName()) {
+				if !contains(namespaceMountedSecrets, secret.GetName()) {
+					println("Secret \"" + secret.GetName() + "\" in namespace \"" + namespace.GetName() + "\" not used")
+				}
+			}
+		}
+
+	} // END - Foreach namespace
+
+	return nil
+}
+
+// func test(clientset kubernetes.Clientset) {
+
+// }
+
+func isDefaultConfigMap(configmap string) bool {
+	defaultConfigMaps := []string{"kube-root-ca.crt", "foobar"}
+
+	for _, value := range defaultConfigMaps {
+		if configmap == value {
+			return true
+		}
+	}
+	return false
+}
+
+func isDefaultSecret(secret string) bool {
+	defaultSecrets := []string{"foobar"}
+
+	for _, value := range defaultSecrets {
+		if secret == value {
+			return true
+		}
+	}
+	return false
 }
